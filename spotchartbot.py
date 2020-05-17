fbd02bc80fcc52f1da60fc586443d1f8fe3f6aea
Caroline Lima <carolflima0@gmail.com>
2020-05-17 17:05:57 -0300

    First spotipy call

--- spotchartbot.py
+++ spotchartbot.py
@@ -3,6 +3,10 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 import logging
 
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+
 token_file = open("token.txt","r")
 token = token_file.read()
 
@@ -32,4 +36,25 @@ caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
 if __name__ == '__main__':
-    updater.start_polling()
+    # updater.start_polling()
+
+    client_credentials_manager = SpotifyClientCredentials(client_id='ac', client_secret='secret')
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    scope = 'user-library-read'
+    token = util.prompt_for_user_token('carolinelima0', scope)
+
+    results = sp.current_user_top_tracks(time_range='short_term', limit=10)
+    for i, item in enumerate(results['items']):
+        print(i, item['name'], '//', item['artists'][0]['name'])
+
+    # playlists = sp.user_playlists('manutunan')
+    # while playlists:
+    #     for i, playlist in enumerate(playlists['items']):
+    #         print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'], playlist['name']))
+    #     if playlists['next']:
+    #         playlists = sp.next(playlists)
+    #     else:
+    #         playlists = None
+
+
